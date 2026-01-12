5ad7febbd97f2f14955238c61425b82226d2383a
Ivan Wu <yupingwu38@gmail.com>
2026-01-12 22:13:05 +0800

    add google analytics

--- ZipGame/ZipGame/ZipGameApp.swift
+++ ZipGame/ZipGame/ZipGameApp.swift
@@ -1,7 +1,19 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct ZipGameApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
